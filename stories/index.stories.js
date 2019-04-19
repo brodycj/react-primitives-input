4e6c136eea1439ae1e1c6b2da3453df5e3b5337c
Christopher J. Brody <chris@brody.consulting>
2019-04-18 21:07:44 -0400

    Add Input story to Storybook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -6,8 +6,15 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+// XXX TBD NOT WORKING in Storybook for some reason:
+// import { Input } from '../index.js'
 
+// TBD TEMPORARY WORKAROUND:
+const Input = require('../TextInput.js')
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Input')} />);
+
+/* ** TBD GONE:
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => (
@@ -17,3 +24,14 @@ storiesOf('Button', module)
       </span>
     </Button>
   ));
+// */
+
+storiesOf('Input', module)
+  .add('with style and placeholder', () => (
+    <Input
+      type='text'
+      style={{height: 48}}
+      placeholder='Type here'
+      placeholder='Type here'
+    />
+  ))
