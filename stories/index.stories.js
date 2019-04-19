960fbc259add4281b55a34d656b4616af1a32caa
Christopher J. Brody <chris@brody.consulting>
2019-04-18 21:37:25 -0400

    Add Input with onChangeText handler story
    with useState hook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Welcome } from '@storybook/react/demo';
+
+import { Text, View } from 'react-primitives'
 
 // XXX TBD NOT WORKING in Storybook for some reason:
 // import { Input } from '../index.js'
@@ -22,3 +24,20 @@ storiesOf('Input', module)
       placeholder='Type some text here'
     />
   ))
+  .add('with onChangeText handler', () => {
+    const MyComponent = () => {
+      const [length, setLength] = useState(0)
+        return (
+          <View>
+            <Input
+              type='text'
+              style={{height: 48}}
+              placeholder='Type some text here'
+              onChangeText={(text) => { setLength(text.length) }}
+            />
+            <Text>{length} characters</Text>
+          </View>
+        )
+    }
+    return <MyComponent/>
+  })
